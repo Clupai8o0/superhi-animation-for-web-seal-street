1e651050bca8f313e7e77c262a1b0b513bb55cc6
Clupai8o0 <clupaio4@gmail.com>
2022-07-31 12:27:05 +0300

    changing rotation on mouse move

--- script.js
+++ script.js
@@ -15,4 +15,12 @@ const observer = new IntersectionObserver(
 
 sections.forEach((section) => {
 	observer.observe(section);
+
+	const div = section.querySelector("div");
+	document.addEventListener("mousemove", (e) => {
+		const aimX = (e.pageX - (window.innerWidth / 2)) / 10;
+		const aimY = (e.pageY - (window.innerHeight / 2)) / 10;
+
+		div.style.transform = `rotateX(${aimY}deg) rotateY(${aimX}deg)`;
+	});
 });
